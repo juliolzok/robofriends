b464b9a3bb599d59466a59926624a17206b8e5ff
Julio Loza <juliolzok@outlook.com>
2018-09-24 18:38:24 -0300

    add styles card component

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({name, jobTitle, id}) => {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 dim bw2 shadow-5'>
-            <img src={`https://robohash.org/${id}?200x200`} alt='robots'/>
+            <img className='br-100 ba bw3 b--light-blue bg-washed-blue grow'  src={`https://robohash.org/${id}?200x200`} alt='robots'/>
             <div className='sans-serif'>
                 <h2 className='fw6 b--black-50'>{name}</h2>
                 <p className='fw3 b--black-50'>{jobTitle}</p>
